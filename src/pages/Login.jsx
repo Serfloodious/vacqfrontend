108d84e33da3b9fc1de47595bc4422009a2258d4
Theerawat Sirisawat <sealactuary128@gmail.com>
2025-10-27 13:21:22 +0700

    Add the rest of code

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,9 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import {toast} from 'react-toastify';
 import {FaSignInAlt} from 'react-icons/fa';
+import {useSelector, useDispatch} from 'react-redux';
+import {login, reset} from '../features/auth/authSlice';
+import {useNavigate} from 'react-router-dom';
 
 function Login() {
     const [formData, setFormData] = useState({
@@ -9,6 +12,24 @@ function Login() {
     });
 
     const {name, email, password, password2} = formData;
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const {user, isLoading, isError, isSuccess, message} = useSelector(state => state.auth);
+
+    useEffect(() => {
+        if(isError) {
+            toast.error(message);
+        }
+
+        // redirect when logged in
+        if(isSuccess || user) {
+            navigate('/');
+        }
+
+        dispatch(reset());
+    }, [user, isError, isSuccess, message, navigate, dispatch]);
 
     const onChange = (e) => {
         setFormData((prevState) => ({
@@ -19,6 +40,13 @@ function Login() {
 
     const onSubmit = (e) => {
         e.preventDefault();
+
+        const userData = {
+            email,
+            password
+        };
+
+        dispatch(login(userData));
     }
 
     return (
